5d702853fede712d9f5b11c0e26fee6c18e432de
vp361533-tech <vp361533@gmail.com>
2026-05-05 21:15:58 -0300

    site,pronto

--- core/settings.py
+++ core/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Variáveis do Aplicativo
-APP_NAME = 'Cadastro de Gente'
-APP_OWNER = 'Luferat'
+APP_NAME = 'Cadastre Aqui'
+APP_OWNER = 'Victor Studios'
 
 # URLs para retorno após login e logout
 LOGIN_REDIRECT_URL = '/'
